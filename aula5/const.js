cedb212fb1cab21b7d57720ae1cd67cbace5c4c2
lucaslmeireles <lluciomeireles@gmail.com>
2022-05-19 20:00:00 +0000

    aula5 gitpod

---
+++ aula5/const.js
@@ -0,0 +1,15 @@
+//Constantes em javascript
+//Para constantes se usa const,nao se pode alterar seu valor
+/*Regras sem palavras reservadas(let,var,console,if,etc), nomes significativos(nomeCliente,idVendedor),
+nao pode começar com numero(1nome), letras minisculas, nao podem conter espaço ou traço, variaveis com nomes compostos usa o 
+camelCase(nomeCompleto,idCliente,telefoneCliente), case-sensitive
+*/
+
+const nome = 'Joao';
+console.log(nome, ' tem 38 anos');
+
+const primeiroNumero = '5';
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado *2;
+console.log(typeof (primeiroNumero + segundoNumero));
