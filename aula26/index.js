3b5b04e9b1a5d63f7c189bf297c61615a5071b76
lucaslmeireles <lluciomeireles@gmail.com>
2022-06-01 18:15:45 +0000

    exercicio gitpod

---
+++ aula26/index.js
@@ -0,0 +1,6 @@
+//operador ternario ?:
+const pontuacaoUsuario = 999;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : 'Usuario Normal';
+//                      condicao             ? verdadeiro    : falso
+
+console.log(nivelUsuario);
