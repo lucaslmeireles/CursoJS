dad8d558439727172a94132d3670eb2f66b8de17
lucaslmeireles <lluciomeireles@gmail.com>
2022-06-05 17:55:08 +0000

    exerci gitpod

--- aula32/model/assets/js/exer.js
+++ aula32/model/assets/js/exer.js
@@ -4,3 +4,22 @@ const elementos = [
     {tag: 'footer', texto: 'Frase 3'},
     {tag: 'section', texto: 'Frase 4'},
 ];
+const container = document.querySelector('.container')
+const div = document.createElement('div');
+
+function recebeElementos(elemento){
+    for (let i= 0; i < elemento.length; i++){
+        const tag = elemento[i];
+        tagETexto(tag);
+    }
+    container.appendChild(div);
+};
+function tagETexto(tag){
+    const {tag: tg, texto: text} = tag;
+    console.log(tg, text);
+    const criaTag = document.createElement(tg);
+    criaTag.innerText = text;
+    div.appendChild(criaTag);
+};
+
+recebeElementos(elementos);
