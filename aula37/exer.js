290ac134cd3fc027be3984f43d1027e0c2fd4a5f
lucaslmeireles <lluciomeireles@gmail.com>
2022-06-06 20:20:46 +0000

    exerc

---
+++ aula37/exer.js
@@ -0,0 +1,8 @@
+//escrever uma função que recebe doi numero e retorna o maior deles 
+
+function retornaMaior(num1, num2){
+    return x> y ? x : y
+    }
+ const max = (x,y) => x > y ? x : y
+
+console.log(max(147,74));
